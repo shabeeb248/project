3880b33d416ee942282a0a4c2d8e97d295ffdbbc
shabeeb248 <96176414+shabeeb248@users.noreply.github.com>
2022-02-12 16:25:36 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,14 +1,12 @@
 import os
 import dash
-import dash_auth
 import dash_core_components as dcc
 import dash_html_components as html
 import plotly.express as px
 import pandas as pd
-from dash.dependencies import input,output
+
 
 app = dash.Dash(__name__)
-
 server = app.server
 
 df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
