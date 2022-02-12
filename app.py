149fbabd41f42d9b77d4c04ca9d7f8ed0e43d3d7
shabeeb248 <96176414+shabeeb248@users.noreply.github.com>
2022-02-12 16:18:55 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,10 +7,8 @@ import plotly.express as px
 import pandas as pd
 from dash.dependencies import input,output
 
-
-
 app = dash.Dash(__name__)
-auth=dash_auth.BasicAuth(app,USERNAME_PASSWORD_PAIRS)
+
 server = app.server
 
 df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
